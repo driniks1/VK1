f556ee4461dbfd9a41f9e382e2d7a531bdb727ff
sunfoxcis <sunfoxcis@gmail.com>
2021-07-06 14:55:07 +0300

    Список постов, средний возраст, интерфейс.

--- vkapplication/vkAPI.Designer.cs
+++ vkapplication/vkAPI.Designer.cs
@@ -34,24 +34,29 @@ namespace vkapplication
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.User = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(32, 203);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Location = new System.Drawing.Point(28, 233);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(337, 27);
+            this.textBox1.Size = new System.Drawing.Size(295, 23);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(47, 412);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(28, 272);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 31);
+            this.button1.Size = new System.Drawing.Size(162, 50);
             this.button1.TabIndex = 1;
             this.button1.Text = "Вывод";
             this.button1.UseVisualStyleBackColor = true;
@@ -60,63 +65,130 @@ namespace vkapplication
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(501, 13);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(438, 10);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(143, 504);
+            this.listBox1.Size = new System.Drawing.Size(126, 379);
             this.listBox1.TabIndex = 3;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 20;
-            this.listBox2.Location = new System.Drawing.Point(691, 13);
-            this.listBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(605, 10);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(149, 504);
+            this.listBox2.Size = new System.Drawing.Size(131, 379);
             this.listBox2.TabIndex = 3;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // User
             // 
             this.User.AutoSize = true;
-            this.User.Location = new System.Drawing.Point(47, 151);
+            this.User.Location = new System.Drawing.Point(28, 194);
             this.User.Name = "User";
-            this.User.Size = new System.Drawing.Size(58, 20);
+            this.User.Size = new System.Drawing.Size(46, 15);
             this.User.TabIndex = 4;
             this.User.Text = "Группа";
             // 
-            // textBox2
+            // listBox3
             // 
-            this.textBox2.Location = new System.Drawing.Point(32, 316);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(337, 27);
-            this.textBox2.TabIndex = 5;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(770, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(213, 274);
+            this.listBox3.TabIndex = 8;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(770, 340);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(213, 49);
+            this.listBox4.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(456, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Имена друзей";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 268);
+            this.label2.Location = new System.Drawing.Point(592, 392);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 20);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Пользователь";
+            this.label2.Size = new System.Drawing.Size(157, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Имена членов сообщества";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(770, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 33);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Возраст каждого друга, установившего дату рождения";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(770, 410);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 33);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Средний возраст пользователя на основе возрастов друзей";
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 15;
+            this.listBox5.Location = new System.Drawing.Point(1021, 10);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(405, 409);
+            this.listBox5.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(1021, 430);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(405, 33);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Список постов пользователя.";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(417, 128);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "VKBOT™";
             // 
             // vkAPI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1162, 664);
+            this.ClientSize = new System.Drawing.Size(1450, 476);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
             this.Controls.Add(this.User);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "vkAPI";
             this.Text = "vkAPI";
             this.ResumeLayout(false);
@@ -131,8 +203,15 @@ namespace vkapplication
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Label User;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- vkapplication/vkAPI.cs
+++ vkapplication/vkAPI.cs
@@ -5,16 +5,20 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using VkNet;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
+using Microsoft.VisualBasic;
 
 namespace vkapplication
 {
+
     public partial class vkAPI : Form
     {
+        int currentYear = DateTime.Now.Year;
         public vkAPI()
         {
             InitializeComponent();
@@ -26,36 +30,22 @@ namespace vkapplication
         }
 
 
-        private string getAuthForGroup()
+        public static string getAuthForUser()
         {
-            if (textBox1.Text == "")
+            string token = "";
+            try
             {
-                MessageBox.Show("Error!");
+                token = File.ReadAllText(@"gaming.txt");
             }
-            return textBox1.Text;
-        }
-        private string getAuthForUser()
-        {
-            if (textBox2.Text == "")
+            catch (Exception)
             {
-                MessageBox.Show("Error!");
+                MessageBox.Show("Message");
             }
-            return textBox2.Text;
+            return token;
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            //var api_group = new VkApi();
-            //try
-            //{
-            //    api_group.Authorize(new ApiAuthParams
-            //    {
-            //        AccessToken = getAuthForGroup()
-            //    });
-            //}
-            //catch (Exception)
-            //{
-            //    MessageBox.Show("Error!");
-            //}
+
             var api_user = new VkApi(); 
             try
             {
@@ -68,17 +58,66 @@ namespace vkapplication
             {
                 MessageBox.Show("Error!");
             }
-            var getFriends = api_user.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+
+            var getFriends = api_user.Friends.Get(new FriendsGetParams
             {
                 Fields = VkNet.Enums.Filters.ProfileFields.All
 
             });
-            foreach (User user in getFriends)
-                listBox1.Items.Add(Encoding.UTF8.GetString(Encoding.GetEncoding(1251).GetBytes(user.FirstName)));
 
-            var ids = api_user.Groups.GetMembers(new GroupsGetMembersParams() { GroupId = textBox1.Text, Fields = VkNet.Enums.Filters.UsersFields.FirstNameAbl});
+
+            var ListofAges = new List<int>();
+            var ActualAges = new List<int>();
+            int averageAge = 0;
+
+            foreach (User user in getFriends)
+            {
+                if (user.BirthDate == null)
+                {
+                    continue;
+                }
+                else if (user.BirthDate.Length > 5)
+                {
+                    int swap = Convert.ToInt32(Strings.Right(user.BirthDate, 4));
+                    ListofAges.Add(swap);
+                }
+                listBox1.Items.Add(Encoding.UTF8.GetString(Encoding.GetEncoding(1251).GetBytes(user.FirstName)));
+            }
+
+            for (int i = 0; i < ListofAges.Count; i++)
+            {
+                int ye = currentYear - ListofAges[i];
+                ActualAges.Add(ye);
+                listBox3.Items.Add(ye);
+            }
+
+
+            for (int i = 0; i < ActualAges.Count; i++)
+            {
+                averageAge += ActualAges[i];
+            }
+
+            averageAge = averageAge / ActualAges.Count;
+
+            listBox4.Items.Add(averageAge);
+
+            var ids = api_user.Groups.GetMembers(new GroupsGetMembersParams() 
+            { 
+                GroupId = textBox1.Text, Fields = VkNet.Enums.Filters.UsersFields.FirstNameAbl
+            });
             foreach (User user in ids)
                 listBox2.Items.Add(Encoding.UTF8.GetString(Encoding.GetEncoding(1251).GetBytes(user.FirstName)));
+
+            var getPosts = api_user.Wall.Get(new WallGetParams{ }) ;
+
+            foreach (var post in getPosts.WallPosts)
+            {
+                listBox5.Items.Add(Encoding.UTF8.GetString(Encoding.GetEncoding(1251).GetBytes(post.Text)));
+                if (post.Attachments.Count > 0)
+                {
+                    listBox5.Items.Add("К этому посту прикреплен файл.");
+                }
+            }
 
             //var get = api_user.Wall.Get(new WallGetParams());
             //foreach (var wallPosts in get.WallPosts)
@@ -86,6 +125,26 @@ namespace vkapplication
         }
 
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vkAPI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void User_Click(object sender, EventArgs e)
         {
 
         }
